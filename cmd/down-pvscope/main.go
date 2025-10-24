42c4287295cb6df14ceba9b2e02ffe7c3d178cd9
Aaron Shifman <aaron@shifman.dev>
2025-10-23 21:08:48 -0400

    Skeleton of pv/pvc lifecycle
    It's a mess but this creates a skeleton of how pvs/pvcs get linked and
    unlined as things are created
    
    Instead of waiting for resource status just sleep instead but close
    enough for a skeleton

--- cmd/down-pvscope/main.go
+++ cmd/down-pvscope/main.go
@@ -3,20 +3,72 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/aaronshifman/down-pvscope/pkg/activities"
 )
 
 func main() {
 	ctx := context.Background()
+
+	// find existing
 	pv, err := activities.GetMatchingPV(ctx, "bogus", "down-pvscope")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(pv)
 
+	// mark existing safe
+	// TODO: store original status and restore at end
 	err = activities.EnsureReclaimPolicyRetain(ctx, pv)
 	if err != nil {
 		panic(err)
 	}
+
+	// create new PVC / provision new PV
+	err = activities.CreateStagingPVC(ctx, "down-pvscope", "bogus", "0.5Gi")
+	if err != nil {
+		panic(err)
+	}
+
+	// find new pv
+	time.Sleep(3 * time.Second) // TODO: hack wait for pvc to provision
+	newPV, err := activities.GetMatchingPV(ctx, "bogus"+activities.StagingSuffix, "down-pvscope")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(newPV)
+
+	// make sure new PV is safe
+	err = activities.EnsureReclaimPolicyRetain(ctx, newPV)
+	if err != nil {
+		panic(err)
+	}
+
+	// TODO: this is where rclone job goes
+
+	// cache original pvc
+	originalSpec, err := activities.GetPVC(ctx, "down-pvscope", "bogus")
+
+	// drop both pvs
+	err = activities.DeletePVC(ctx, "down-pvscope", "bogus")
+	if err != nil {
+		panic(err)
+	}
+	err = activities.DeletePVC(ctx, "down-pvscope", "bogus"+activities.StagingSuffix)
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(3 * time.Second) // TODO: again hack for wait to delete
+
+	// map the new pv to the original pvc
+	fmt.Println("Rebinding PVC")
+	err = activities.RebindPV(ctx, "down-pvscope", newPV, originalSpec, "0.5Gi")
+	if err != nil {
+		panic(err)
+	}
+
+	// TODO: optionally drop the original pv
+
+	time.Sleep(10000 * time.Hour)
 }
--- pkg/activities/pvc.go
+++ pkg/activities/pvc.go
@@ -2,11 +2,16 @@ package activities
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/aaronshifman/down-pvscope/pkg/util"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+const StagingSuffix = "-staging"
 
 func GetMatchingPV(ctx context.Context, pvcName, ns string) (string, error) {
 	client, err := util.GetClientset()
@@ -38,4 +43,94 @@ func EnsureReclaimPolicyRetain(ctx context.Context, pvName string) error {
 		_, err = client.CoreV1().PersistentVolumes().Update(ctx, pv, metav1.UpdateOptions{})
 	}
 	return err
+}
+
+func CreateStagingPVC(ctx context.Context, namespace, name, size string) error {
+	client, err := util.GetClientset()
+	if err != nil {
+		return err
+	}
+
+	pvc := &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{Name: name + StagingSuffix},
+		Spec: corev1.PersistentVolumeClaimSpec{
+			AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
+			Resources: corev1.VolumeResourceRequirements{
+				Requests: corev1.ResourceList{"storage": resource.MustParse(size)},
+			},
+		},
+	}
+	_, err = client.CoreV1().PersistentVolumeClaims(namespace).Create(ctx, pvc, metav1.CreateOptions{})
+	return err
+}
+
+func DeletePVC(ctx context.Context, namespace, pvcName string) error {
+	client, err := util.GetClientset()
+	if err != nil {
+		return err
+	}
+
+	return client.CoreV1().PersistentVolumeClaims(namespace).Delete(ctx, pvcName, metav1.DeleteOptions{})
+}
+
+func RebindPV(ctx context.Context, namespace, pvName string, origPVC *corev1.PersistentVolumeClaim, newSize string) error {
+	client, err := util.GetClientset()
+	if err != nil {
+		return err
+	}
+
+	pv, err := client.CoreV1().PersistentVolumes().Get(ctx, pvName, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	// unlink existing volume
+	fmt.Println("Unlinking existing volume")
+	pv.Spec.ClaimRef = nil
+	_, err = client.CoreV1().PersistentVolumes().Update(ctx, pv, metav1.UpdateOptions{})
+	if err != nil {
+		return err
+	}
+	time.Sleep(1 * time.Second)
+
+	// create new PVC
+	fmt.Println("Creating cloned pvc")
+	origPVC.Spec.VolumeName = pvName
+	newPVC := &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        origPVC.Name,
+			Namespace:   origPVC.Namespace,
+			Labels:      origPVC.Labels,
+			Annotations: origPVC.Annotations,
+		},
+		Spec: corev1.PersistentVolumeClaimSpec{
+			AccessModes:      origPVC.Spec.AccessModes,
+			StorageClassName: origPVC.Spec.StorageClassName,
+			VolumeName:       pvName,
+			Resources: corev1.VolumeResourceRequirements{
+				Requests: corev1.ResourceList{"storage": resource.MustParse(newSize)},
+			},
+		},
+	}
+
+	_, err = client.CoreV1().PersistentVolumeClaims(namespace).Create(ctx, newPVC, metav1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func GetPVC(ctx context.Context, namespace, pvcName string) (*corev1.PersistentVolumeClaim, error) {
+	client, err := util.GetClientset()
+	if err != nil {
+		return nil, err
+	}
+
+	pvc, err := client.CoreV1().PersistentVolumeClaims(namespace).Get(ctx, pvcName, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	return pvc, err
 }
